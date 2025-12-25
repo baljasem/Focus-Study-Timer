e100308ad75b07685c927829f84d242d6c6daa03
baljasem <bahaa.aljasem96@gmail.com>
2025-12-25 20:29:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = "focus-study-timer-v3";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
