ec3fb8c6add5a1f916710f2413f8ce5a69db987b
baljasem <bahaa.aljasem96@gmail.com>
2025-12-25 19:59:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,9 @@
-const CACHE_NAME = 'focus-study-timer-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  console.log('Service Worker installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener("activate", e => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
