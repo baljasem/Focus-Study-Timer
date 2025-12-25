d4bdd900bc36579e6cf30a7e9efa82a55446f7fd
baljasem <bahaa.aljasem96@gmail.com>
2025-12-25 02:55:54 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', e => {
+  // You can leave this empty for now
+});
